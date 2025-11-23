3821fc5c0ce016cfdb8aa7412dbeb6c5e59ac6d2
yvesls <yveslimasilva@gmail.com>
2025-11-23 12:59:27 -0300

    refactor: correção da repetição que pode levar a um estouro de pilha para entradas grandes.

--- backend/src/main/java/com/ethicalsoft/ethicalsoft_complience/util/validators/EmailValidator.java
+++ backend/src/main/java/com/ethicalsoft/ethicalsoft_complience/util/validators/EmailValidator.java
@@ -9,7 +9,9 @@ import java.util.regex.Pattern;
 
 public class EmailValidator implements ConstraintValidator<Email, String> {
 
-	private static final Pattern EMAIL_PATTERN = Pattern.compile( "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$" );
+	private static final Pattern EMAIL_PATTERN = Pattern.compile(
+			"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
+	);
 
 	@Override
 	public boolean isValid( String value, ConstraintValidatorContext context ) {
@@ -20,6 +22,9 @@ public class EmailValidator implements ConstraintValidator<Email, String> {
 	}
 
 	private boolean isValidEmailFormat( String email ) {
+		if (email == null || email.length() > 256) {
+			return false;
+		}
 		return EMAIL_PATTERN.matcher( email ).matches();
 	}
 
