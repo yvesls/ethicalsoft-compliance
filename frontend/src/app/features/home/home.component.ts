10165a3bd820db0d8e2ba8be328144f6d9a3816b
yvesls <yveslimasilva@gmail.com>
2026-06-17 17:32:07 -0300

    Criando tela de  orientação no home do projeto

--- frontend/src/app/features/home/home.component.ts
+++ frontend/src/app/features/home/home.component.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
-import { NgxSpinnerModule } from 'ngx-spinner'
 
 @Component({
 	selector: 'app-home',
-	imports: [CommonModule, NgxSpinnerModule],
+	standalone: true,
+	imports: [CommonModule],
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.scss'],
 })
